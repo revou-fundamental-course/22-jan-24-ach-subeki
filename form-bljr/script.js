15a092f12811ffcd36bb41a6459cab4c40a7d6f1
ach-subeki <157862536+ach-subeki@users.noreply.github.com>
2024-01-31 11:14:43 +0700

    mengerjakan tabel

--- form-bljr/script.js
+++
@@ -1,7 +0,0 @@
-const jenisKelamin = document.querySelector('input[name="jenis_kelamin"]:checked').value;
-
-if (jenisKelamin === 'laki-laki') {
-  console.log('Jenis kelamin: laki-laki');
-} else {
-  console.log('Jenis kelamin: perempuan');
-}
